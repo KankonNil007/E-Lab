24cf55ea6b0eedb3605bbcd688217c8197e3786f
Kankon Mondal <kankonnil@gmail.com>
2026-09-12 00:53:53 +0600

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-lab/',
+  base: '/E-Lab/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
